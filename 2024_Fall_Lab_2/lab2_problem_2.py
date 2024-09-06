378e06da61b909316cf79068d48f965decd61557
SanjanaMayenkar <34608219+SanjanaMayenkar@users.noreply.github.com>
2024-09-06 14:54:46 -0400

    calculated delay between M2 and M1

--- 2024_Fall_Lab_2/lab2_problem_2.py
+++ 2024_Fall_Lab_2/lab2_problem_2.py
@@ -4,11 +4,8 @@ import numpy as np
 import math
 
 def cross_corr(x, y):
-    R = []
-    R = np.array(R, dtype=np.int32)
+    R = np.zeros(x.size, dtype=np.int64)
     for m in range(x.size):
-        np.append(R, [0], axis=0)
-        print(R)
         for n in range(x.size):
             R[m] += (x[n] * y[n-m])
     return R
@@ -36,5 +33,32 @@ print(data1_rms)
 print(data2_rms)
 print(data3_rms)
 
-R = cross_corr(data1, data2)
-print(R)
+# R = cross_corr(data1, data2)
+# print(R)
+
+max_time1 = data1.size/samplerate1
+time_steps1 = np.linspace(0, max_time1, data1.size)
+max_time2 = data2.size/samplerate2
+time_steps2 = np.linspace(0, max_time2, data2.size)
+
+plt.plot(time_steps1, data1, 'b')
+plt.plot(time_steps2, data2, 'r')
+plt.ylabel("Amplitude")
+plt.xlabel("Time (s)")
+plt.title("Plot of M1 (blue) and M2 (red)")
+#plt.show()
+
+
+i = 0
+while data1[i] == 0:
+    i += 1
+time1 = i/samplerate1
+
+i = 0
+while data2[i] == 0:
+    i += 1
+time2 = i/samplerate2
+
+delay = time2 - time1
+
+print(delay)
