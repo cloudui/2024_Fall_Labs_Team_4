8c2c868b403f8d244cdbb5d785bb3d1730a77597
SanjanaMayenkar <34608219+SanjanaMayenkar@users.noreply.github.com>
2024-10-08 13:14:11 -0400

    camera.py working

--- 2024_Fall_Lab_5/camera.py
+++ 2024_Fall_Lab_5/camera.py
@@ -3,25 +3,24 @@ import time
 import matplotlib.pyplot as plt
 
 cam = cv2.VideoCapture(0)
+i = 0
 
 while(True):
     result, face_image = cam.read()
 
-    if result:
-        plt.imshow(face_image)
-        plt.show()
-
+    if result and i != 0:
         cv2.imshow('image', face_image)
-        
-        cv2.waitKey(5000)
-        time.sleep(5)
+        time.sleep(2)
+        cv2.waitKey(10)
 
         face_image_gray = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray_image', face_image_gray)
+        time.sleep(2)
         cv2.waitKey(10)
 
-        haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontal_face_default.xml")
+        haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         cv2.waitKey(10)
-        faces = haar_cascade.detectMultiScale(face_image_gray)
+        faces = haar_cascade.detectMultiScale(face_image_gray, 1.3, 5)
 
 
         for face in faces:
@@ -31,6 +30,10 @@ while(True):
                 cv2.circle(face_image, (center_x, center_y), int(face[2] / 2), (255,0,0), 10)
 
         cv2.imshow('detect faces', face_image)
-        time.sleep(2)
+        time.sleep(5)
         cv2.waitKey(10)
 
+        time.sleep(5)
+        cv2.destroyAllWindows()
+    
+    i = 1
